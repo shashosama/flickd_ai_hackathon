1f0b8c799d9a1b7062599f2cdb79daf08eb3b5db
sahashashota <sahashashota@gmail.com>
2025-06-03 22:33:02 +0000

    vibe

--- models/vibe_classifier.py
+++ models/vibe_classifier.py
@@ -1,13 +1,15 @@
-from transformers import pipeline
+from transformers import pipeline # importing libraries 
 
-# Define supported vibes
+# all the  vibes 
 VIBES = ["Coquette", "Clean Girl", "Cottagecore", "Streetcore", "Y2K", "Boho", "Party Glam"]
 
 # Load zero-shot classifier pipeline
+#the model did not see training data for these vibes but can read the caption and say : cottage and boho 
 classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 
+#function that reads and looks at the captoon or transcript from a video
 def classify_vibe(text):
-    result = classifier(text, candidate_labels=VIBES, multi_label=True)
+    result = classifier(text, candidate_labels=VIBES, multi_label=True)#multi_label = true so it can pick more than one if it wants 
     # Return top 3 vibes with score > 0.3
     vibes = [label for label, score in zip(result["labels"], result["scores"]) if score > 0.3]
-    return vibes[:3]
+    return vibes[:3]#sends back top 3 matching vibes for this video 

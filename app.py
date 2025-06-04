228b2f1d6ca1996834f162247d51ef3eaaf6e262
sahashashota <sahashashota@gmail.com>
2025-06-04 04:01:39 +0000

    render

--- app.py
+++ app.py
@@ -26,4 +26,5 @@ def process_video():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  port = int(os.environ.get("PORT", 5000))  # Use Render's port or fallback to 5000
+    app.run(host="0.0.0.0", port=port)

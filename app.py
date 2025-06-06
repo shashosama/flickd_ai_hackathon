45e3d67b35a4d946b382eccab06869d3a84ab011
shashosama <160487546+shashosama@users.noreply.github.com>
2025-06-06 05:36:31 +0000

    cool

--- app.py
+++ app.py
@@ -61,5 +61,8 @@ def upload_video():
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT
+    app.run(host="0.0.0.0", port=port, debug=True)
+
